6eccfe1d45c741f5d3275ac81e014e90254681ab
zeyadrezk <elzooz@students.mans.edu.eg>
2023-10-13 21:49:48 +0300

    updated 1

---
+++ resources/views/invoices/invoices.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.master')
+@section('css')
+	<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+	<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+	<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+
+@endsection
+@section('page-header')
+				<!-- breadcrumb -->
+				<div class="breadcrumb-header justify-content-between">
+					<div class="my-auto">
+						<div class="d-flex">
+							<h4 class="content-title mb-0 my-auto">الفواتير</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمة الفواتير</span>
+						</div>
+					</div>
+
+
+
+				</div>
+				<!-- breadcrumb -->
+@endsection
+@section('content')
+				<!-- row -->
+					<div class="row row-sm">
+						<div class="col-xl-12">
+							<div class="card">
+								<div class="card-header pb-0">
+									<div class="d-flex justify-content-between">
+										<h4 class="card-title mg-b-0">SIMPLE TABLE</h4>
+										<i class="mdi mdi-dots-horizontal text-gray"></i>
+									</div>
+									<p class="tx-12 tx-gray-500 mb-2">Example of Valex Simple Table. <a href="">Learn more</a></p>
+								</div>
+								<div class="card-body">
+									<div class="table-responsive">
+										<table class="table text-md-nowrap" id="example1">
+											<thead>
+											<tr>
+												<th class="border-bottom-0">#</th>
+												<th class="border-bottom-0">رقم الفاتورة</th>
+												<th class="border-bottom-0">تاريخ القاتورة</th>
+												<th class="border-bottom-0">تاريخ الاستحقاق</th>
+												<th class="border-bottom-0">المنتج</th>
+												<th class="border-bottom-0">القسم</th>
+												<th class="border-bottom-0">الخصم</th>
+												<th class="border-bottom-0">نسبة الضريبة</th>
+												<th class="border-bottom-0">قيمة الضريبة</th>
+												<th class="border-bottom-0">الاجمالي</th>
+												<th class="border-bottom-0">الحالة</th>
+												<th class="border-bottom-0">ملاحظات</th>
+											</tr>
+											</thead>
+											<tbody>
+											<tr>
+												<td>1</td>
+												<td>2161</td>
+												<td>2020-09-13</td>
+												<td>2021/03/20</td>
+												<td>cc</td>
+												<td>البنك الاهلي</td>
+												<td>2500</td>
+												<td>1000</td>
+												<td>10%</td>
+												<td>2590</td>
+												<td>غير مدفوعة</td>
+												<td>لم يتم السداد</td>
+											</tr>
+
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--/div-->
+
+					</div>
+
+				</div>
+				<!-- row closed -->
+			</div>
+			<!-- Container closed -->
+		</div>
+		<!-- main-content closed -->
+@endsection
+@section('js')
+	<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+	<!--Internal  Datatable js -->
+	<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
+	
+	use App\Http\Controllers\InvoicesController;
+	use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,17 +17,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+	Route::get('/home', function () {
+		return view('home');
+	})->middleware(['auth', 'verified'])->name('home');
+	
+	Route::get('/invoices', [InvoicesController::class,'index'])->name('invoices.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+	
+	
+	
+	
+	
+	
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-	Route::get('/{page}', [AdminController::class,'index']);
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+	
+//	Route::get('/{page}', [AdminController::class,'index']);
 require __DIR__.'/auth.php';
